0d04d70aa36a3b8319eb975d57e0313e2b7461f0
Jaivanth Koppula <jaivanthkoppula999@gmail.com>
2025-09-01 13:23:41 +0530

    first commit

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // ✅ backend URL
+  baseURL: "https://notesapp1-7ty0.onrender.com/api", // ✅ backend URL
   withCredentials: true, // allow cookies
 });
 
