c3b905db249a182cd138ab593e619ab406e68038
Jaivanth Koppula <jaivanthkoppula999@gmail.com>
2025-09-01 13:39:55 +0530

    first commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,7 @@ import noteRoutes from "./routes/notes";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://notesappzz.vercel.app/", credentials: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 
